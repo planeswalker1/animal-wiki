6a63cb4ae822bae65b5d69108bf6527510af2fff
dan <daniel.munozdev@gmail.com>
2017-11-14 18:24:42 -0800

    Add descriptive comments and stop event bubbling from links

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
+//Everything below this line is user interface (or front-end) logic:
+// img div selector
 $('.initially-shown').click(function() {
   $(this).slideToggle();
   $(this).next().slideToggle();
 });
-
+// description text div selector
 $('.initially-hidden').click(function() {
   $(this).slideToggle();
   $(this).prev().slideToggle();
+});
+// stop event bubbling from clicking on a link
+$('a').click(function (event) {
+  event.stopPropagation();
 });
